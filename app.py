713fcdda246770f351996b342224e5f90c690dd7
CodeDecode <72653540+CodeDecodeDev@users.noreply.github.com>
2021-06-01 22:54:41 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+import math
+
+
+print("Hello! Welcome to Quad Equation Solver 0.01!")
+print("A basic Quad Equation looks like a*x^2+b*x+c")
+print("Please input the values for A, B and C!")
+
+a_value = int(input())
+b_value = int(input())
+c_value = int(input())
+
+a = a_value
+b = b_value
+c = c_value
+
+answers = set() 
+
+try:
+
+    answers.add(
+        ((-1*b)+math.sqrt((b*b)-4*a*c))/2*a
+    )
+
+    answers.add(
+        ((-1*b)-math.sqrt((b*b)-4*a*c))/2*a
+    )
+
+except:
+    print("The equation has no solution!")
+
+if not answers == set():
+    print(answers)
